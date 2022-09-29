91cb819fd95e99e1f889857a43d5b31497dfc62a
Khadeer <114642516+KhadeerGithub@users.noreply.github.com>
2022-09-29 12:05:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
-streamlit.title('Arhaa lazy girl') 
+streamlit.title('Arhaa - Lazy girl') 
+
+streamlit.Header('School Details') 
+streamlit.text('Calss Jr.KG')
+streamlit.text('IWS School')
+streamlit.text('Pune')
