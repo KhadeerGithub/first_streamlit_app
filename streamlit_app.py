434417f562892c1a7e06e4356d23eabd31cb721a
Khadeer <114642516+KhadeerGithub@users.noreply.github.com>
2022-10-05 18:52:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,42 +51,4 @@ if not fruit_choice:
     stramlit.error()
 
  
-  streamlit.write('The user entered ', fruit_choice)
-
-
-
-
-
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"fruit_choice")
-
-
-streamlit.text(fruityvice_response)
-stramlit.stop()
-# write your own comment -what does the next line do? 
-
-# write your own comment - what does this do?
-
-
-
-
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_row = my_cur.fetchall()
-streamlit.text("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
-
-fruit_choice = streamlit.text_input('What fruit would you like add?',fruit_choice)
-streamlit.write('Thanks for adding ', fruit_choice)
-
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-
-                                   
+  
