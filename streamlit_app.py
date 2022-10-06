02125105ca7f276718595a8b6828b80510166316
Khadeer <114642516+KhadeerGithub@users.noreply.github.com>
2022-10-06 11:04:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 # To import the csv file
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 # Display the total table on the page.
@@ -19,4 +20,5 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?')
 streamlit.error("Please select a fruit to get information")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/kiwi")
-
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
