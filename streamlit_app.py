72a13fb17f91ce971bb74a6986636d35b7b6282d
Khadeer <114642516+KhadeerGithub@users.noreply.github.com>
2022-10-06 10:46:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,11 @@
 import streamlit
+streamlit.title('Arhaa - Lazy girl') 
+streamlit.header('School Details') 
+streamlit.text('Calss Jr.KG')
+streamlit.text('IWS School')
+streamlit.text('Pune')
+streamlit.header('Breakfast Menu') 
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal') 
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
