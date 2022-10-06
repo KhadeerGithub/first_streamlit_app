a828ce02e1fe0521c5c72884ea7a8e8cbdf8fa87
Khadeer <114642516+KhadeerGithub@users.noreply.github.com>
2022-10-06 11:03:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,4 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?')
 streamlit.error("Please select a fruit to get information")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/kiwi")
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
 
