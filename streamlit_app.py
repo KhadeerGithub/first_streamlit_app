d28bb2d7c7a7ba72512252eeb725411f2f057ec4
Khadeer <114642516+KhadeerGithub@users.noreply.github.com>
2022-10-06 11:21:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 import pandas
 import requests
+import snowflake.connector
+from urllib.error import URLError
 # To import the csv file
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 # Display the total table on the page.
